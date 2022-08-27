9f721c54fb0085ade064f7bcd60c97d3f323bf57
PRABHATI GOUDA <gaudaprabhati12@gmail.com>
2022-08-27 21:00:02 +0530

    fc

---
+++ BlockNetworkRequests.java
@@ -0,0 +1,31 @@
+import java.util.Optional;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v102.network.Network;
+
+import com.google.common.collect.ImmutableList;
+
+public class BlockNetworkRequests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\PRABHATI GOUDA\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        DevTools devTools = driver.getDevTools();
+        devTools.createSession();
+        
+        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+        devTools.send(Network.setBlockedURLs(ImmutableList.of("*.jpg","*.css")));
+        long startTime = System.currentTimeMillis();
+        driver.get("https://rahulshettyacademy.com/angularAppdemo");
+        driver.findElement(By.linkText("Selenium")).click();
+        driver.findElement(By.cssSelector(".add-to-cart")).click();
+        System.out.println(driver.findElement(By.cssSelector("p")).getText());
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+        
+	}
+
+}
